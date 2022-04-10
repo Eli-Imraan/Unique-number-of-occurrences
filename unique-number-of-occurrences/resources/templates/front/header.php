f7d1a728e5cdd5970d71effe29eeb7b30f36d2f7
Eli Imraan <80774610+Eli-Imraan@users.noreply.github.com>
2022-04-10 09:06:30 +0800

    Add files via upload

---
+++ unique-number-of-occurrences/resources/templates/front/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Unique number of occurrences</title>
+
+    <!-- Font Awesome CSS -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="js/scripts.js"></script>
+
+    <link href="css/styles_login.css" rel="stylesheet">
+    <?php include(TEMPLATE_FRONT . DS . "top_nav.php") ?>
+</head>
+
+
+
